0f6e16cdb2022e45b0a52f7292ef84dfa98ac862
Raimy <raimy241@gmail.com>
2023-03-31 12:13:57 -0400

    cambio en movieData

--- assets/js/movieData.js
+++ assets/js/movieData.js
@@ -3,11 +3,11 @@ export const movieData = [
         nombre: 'John Wick',
         video: 'yjRHZEUamCc',
         img: 'https://m.media-amazon.com/images/M/MV5BMDExZGMyOTMtMDgyYi00NGIwLWJhMTEtOTdkZGFjNmZiMTEwXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_FMjpg_UY6001_.jpg',
-        sinopsis: 'Después de los eventos de Spider-Man No Way Home, el Doctor Strange, sin darse cuenta, lanza un hechizo prohibido que accidentalmente abre el multiverso. Con la ayuda de Wong y Scarlet Witch, Strange se enfrenta a varias versiones de sí mismo y se une al joven América Chavez mientras viaja a través de varias realidades y trabaja para restaurar la realidad tal como la conoce. En el camino, Strange y sus aliados se dan cuenta de que deben enfrentarse a un nuevo y poderoso adversario que busca apoderarse del multiverso.',
-        reparto: 'Benedict Cumberbatch, Elizabeth Olsen, Chiwetel Ejiofor',
-        direccion: 'Sam Raimi',
-        guion: 'Michael Waldron, Stan Lee,Steve Ditko',
-        generos: 'Acción, Aventura, Fantasía, Terror, Ciencia ficción'
+        sinopsis: 'John Wick descubre un camino para derrotar a La Alta Mesa. Pero antes de que pueda ganar su libertad, Wick debe enfrentarse a un nuevo enemigo con poderosas alianzas en todo el mundo y fuerzas que convierten aviejos amigos en enemigos.',
+        reparto: 'Keanu Reeves, Laurence Fishburne, George Georgiou',
+        direccion: 'Chad Stahelski',
+        guion: 'Shay Hatten, Michael Finch, Derek Kolstad',
+        generos: 'Acción, Crimen, Suspenso'
     },
     {
         nombre: 'Doctor Strange en el multiverso de la locura',
