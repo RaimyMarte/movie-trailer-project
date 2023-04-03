46c1c3d962adadc32ef7b16d62051b01861bb698
gomez118 <64871883+gomez118@users.noreply.github.com>
2023-04-03 10:56:14 -0400

    Added genres to index.html

--- assets/js/movieData.js
+++ assets/js/movieData.js
@@ -1,3 +1,26 @@
+const genres = [
+    "Comedy",
+    "Fantasy",
+    "Crime",
+    "Drama",
+    "Music",
+    "Adventure",
+    "History",
+    "Thriller",
+    "Animation",
+    "Family",
+    "Mystery",
+    "Biography",
+    "Action",
+    "Film-Noir",
+    "Romance",
+    "Sci-Fi",
+    "War",
+    "Western",
+    "Horror",
+    "Musical",
+    "Sport"]
+
 export const movieData = [
     {
         nombre: 'John Wick',
